b0efed417091b42b15c0addd70da5e393e8cc861
Kyle Cesare <kcesare@gmail.com>
2013-11-05 18:05:43 -0800

    Add code to load video from file.

---
+++ src/vision.c
@@ -0,0 +1,36 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cv.h>
+#include <highgui.h>
+
+int main(int argc, char **argv) {
+  if(argc < 2) {
+    fprintf(stderr, "Usage: %s [file_name]\n", argv[0]);
+    return 0;
+  }
+
+  cvNamedWindow("img_win", CV_WINDOW_AUTOSIZE);
+
+  CvCapture* cap = cvCaptureFromFile(argv[1]);
+  while(true) {
+    CvArr *frame = cvQueryFrame(cap);
+
+    if(frame != NULL) {
+      cvShowImage("img_win", frame);
+    } else {
+      fprintf(stderr, "No frame...\n");
+    }
+
+    char key = cvWaitKey(30);
+    if(key && key == 27) {
+      break;
+    }
+  }
+
+  cvReleaseCapture(&cap);
+
+  return 0;
+}
