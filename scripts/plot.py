39637144f5c7a912a61558da36eebd61b1f7ac2c
Kyle Cesare <kcesare@gmail.com>
2014-01-17 16:36:54 -0800

    Fix plotting tool for HLS.

--- scripts/plot.py
+++ scripts/plot.py
@@ -4,9 +4,10 @@ import matplotlib.pyplot as plt
 
 fname = sys.argv[1]
 
-data = np.genfromtxt(fname, delimiter=',', names=['time', 'b', 'g', 'r'])
+data = np.genfromtxt(fname, delimiter=',', names=['h', 'l', 's', 'time'])
 
-plt.plot(data['time'], data['b'], 'b,')
-plt.plot(data['time'], data['g'], 'g,')
-plt.plot(data['time'], data['r'], 'r,')
+plt.plot(data['time'], data['h'], 'b,')
+plt.plot(data['time'], data['l'], 'g,')
+plt.plot(data['time'], data['s'], 'r,')
+
 plt.show()
