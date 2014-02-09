19226eba4cd0e2f74002e859db28b87f1fd1c2a6
Kyle Cesare <kcesare@gmail.com>
2014-02-08 18:10:08 -0800

    Add configuration options.

--- include/BiomarkerImageProcessor.h
+++ include/BiomarkerImageProcessor.h
@@ -10,9 +10,21 @@ public:
   void reset();
   cv::Scalar process(cv::Mat frame);
   std::vector<cv::Scalar> getSamples() { return samples; }
+
+  /* Options */
+  bool isCircleDetectionEnabled() { return this->circleDetectionEnabled; }
+  void setCircleDetectionEnabled(bool c) { this->circleDetectionEnabled = c; }
+  float getCircleCenterX() { return this->circleCenterX; }
+  float getCircleCenterY() { return this->circleCenterY; }
+  void setCircleCenterX(float x) { this->circleCenterX = x; }
+  void setCircleCenterY(float y) { this->circleCenterY = y; }
+
 private:
   boost::timer::cpu_timer timer;
   std::vector<cv::Scalar> samples;
+  bool circleDetectionEnabled;
+  float circleCenterX, circleCenterY;
+  float circleRadius;
 
   cv::Vec3f findSampleCircle(cv::Mat frame);
   cv::Scalar sampleSlide(cv::Mat frame, cv::Vec3f sampleCircle);
--- src/BiomarkerImageProcessor.cpp
+++ src/BiomarkerImageProcessor.cpp
@@ -13,16 +13,20 @@
 
 /* Predefined circle locations to use when circle detection is disabled.
  */
-#define CIRCLE_CENTER_X      317.5
-#define CIRCLE_CENTER_Y      237.5
-#define CIRCLE_CENTER_RADIUS 30.0
+#define DEFAULT_CIRCLE_CENTER_X      317.5
+#define DEFAULT_CIRCLE_CENTER_Y      237.5
+#define DEFAULT_CIRCLE_RADIUS        30.0
 
 /* Enable or disable circle detection. Will use predefined values above if
  * disabled.
  */
 // #define CIRCLE_DETECTION
 
-BiomarkerImageProcessor::BiomarkerImageProcessor() {
+BiomarkerImageProcessor::BiomarkerImageProcessor() :
+  circleCenterX(DEFAULT_CIRCLE_CENTER_X),
+  circleCenterY(DEFAULT_CIRCLE_CENTER_Y),
+  circleRadius(DEFAULT_CIRCLE_RADIUS),
+  circleDetectionEnabled(false) {
 }
 
 void BiomarkerImageProcessor::reset() {
@@ -30,11 +34,12 @@ void BiomarkerImageProcessor::reset() {
 }
 
 cv::Scalar BiomarkerImageProcessor::process(cv::Mat frame) {
-#ifdef CIRCLE_DETECTION
-  cv::Vec3f sampleCircle = this->findSampleCircle(frame);
-#else
-  cv::Vec3f sampleCircle = cv::Vec3f(CIRCLE_CENTER_X, CIRCLE_CENTER_Y, CIRCLE_CENTER_RADIUS);
-#endif
+  cv::Vec3f sampleCircle;
+  if(isCircleDetectionEnabled()) {
+    sampleCircle = this->findSampleCircle(frame);
+  } else {
+    sampleCircle = cv::Vec3f(circleCenterX, circleCenterY, circleRadius);
+  }
 
 #ifdef DEBUG_MODE
   cv::circle(frame, cv::Point(sampleCircle[0], sampleCircle[1]), sampleCircle[2], cv::Scalar(255, 0, 0), 1, 8, 0);
