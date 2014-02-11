5a6b6b958671a1e4af581ac39c75059c06c06de7
Kyle Cesare <kcesare@gmail.com>
2014-02-10 16:36:10 -0800

    Move main out of BiomarkerImageProcessor.

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CXXFLAGS=-O2 -Iinclude
 LDFLAGS=`pkg-config --cflags opencv` `pkg-config --libs opencv` -lboost_system -lboost_timer
 
 default:
-	$(CXX) $(CXXFLAGS) -o vision src/BiomarkerImageProcessor.cpp $(LDFLAGS)
+	$(CXX) $(CXXFLAGS) -o vision src/BiomarkerImageProcessor.cpp src/imgtest.cpp $(LDFLAGS)
 regr:
 	$(CXX) $(CXXFLAGS) -o regression src/ModelComponent.cpp src/RegressionModel.cpp src/RegressionFactory.cpp src/regrtest.cpp $(LDFLAGS)
 
---
+++ src/imgtest.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+
+#include "BiomarkerImageProcessor.h"
+
+#define FRAME_SKIP 0
+
+/*
+ * Skip n frames returning the n+1 frame. Returns NULL if no frames are
+ * available.
+ */
+bool skipNFrames(cv::VideoCapture capture, int n, cv::Mat *image) {
+  for(int i = 0; i < n + 1; i++) {
+    if(!capture.read(*image)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main(int argc, char **argv) {
+  if(argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " [file_name]" << std::endl;
+    return 0;
+  }
+
+  BiomarkerImageProcessor processor;
+
+  cv::namedWindow("img_win", CV_WINDOW_AUTOSIZE);
+  cv::VideoCapture cap(argv[1]);
+
+  cv::Mat frame;
+  // skipNFrames(cap, 8730, &frame);
+  while(true) {
+    bool got_frame = skipNFrames(cap, FRAME_SKIP, &frame);
+
+    if(got_frame) {
+      cv::Scalar sample = processor.process(frame);
+      std::cout << sample[0] << "," << sample[1] << "," << sample[2] << "," << sample[3] << std::endl;
+
+      cv::imshow("img_win", frame);
+    } else {
+      break;
+    }
+
+    char key = cvWaitKey(1);
+    if(key == 27) {
+      break;
+    }
+  }
+
+  return 0;
+}
