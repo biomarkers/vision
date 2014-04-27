69c322ba79e148a436aefb3e73694134498b4398
Kyle Cesare <kcesare@gmail.com>
2014-04-26 17:32:08 -0700

    Add header guards.

--- include/vision/CircularSampleAreaDetector.h
+++ include/vision/CircularSampleAreaDetector.h
@@ -1,3 +1,6 @@
+#ifndef __CIRCULAR_SAMPLE_AREA_DETECTOR_H
+#define __CIRCULAR_SAMPLE_AREA_DETECTOR_H
+
 #include "vision/SampleAreaDetector.h"
 
 #define CIRCLE_SHRINK_FACTOR 0.85
@@ -6,3 +9,5 @@ class CircularSampleAreaDetector : SampleAreaDetector<cv::Vec3f> {
 public:
   std::vector<cv::Vec3f> detect(cv::Mat frame);
 };
+
+#endif
--- include/vision/SampleAreaDetector.h
+++ include/vision/SampleAreaDetector.h
@@ -1,3 +1,6 @@
+#ifndef __SAMPLE_AREA_DETECTOR_H
+#define __SAMPLE_AREA_DETECTOR_H
+
 #include <vector>
 
 #include <opencv2/imgproc/imgproc.hpp>
@@ -7,3 +10,5 @@ class SampleAreaDetector {
 public:
   virtual std::vector<T> detect(cv::Mat frame) = 0;
 };
+
+#endif
