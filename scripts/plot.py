4d35250a4d2950450bbcbe2bda354431611b1e6f
Kyle Cesare <kcesare@gmail.com>
2014-05-07 17:31:17 -0700

    Plot stddev on each channel.

--- scripts/plot.py
+++ scripts/plot.py
@@ -12,6 +12,10 @@ plt.plot(data['time'], data['r'], 'r,')
 
 plt.fill_between(data['time'], data['b'] + data['stddev'], data['b'] -
         data['stddev'], facecolor='blue', alpha=0.2)
+plt.fill_between(data['time'], data['g'] + data['stddev'], data['g'] -
+        data['stddev'], facecolor='green', alpha=0.2)
+plt.fill_between(data['time'], data['r'] + data['stddev'], data['r'] -
+        data['stddev'], facecolor='red', alpha=0.2)
 
 plt.title(fname)
 
