f9c12609508e885620fbd768d52e1c72d2f0761b
Kyle Cesare <kcesare@gmail.com>
2014-05-14 16:10:18 -0700

    Add statistics column to result table in database.

--- include/persistence/ResultEntry.h
+++ include/persistence/ResultEntry.h
@@ -13,8 +13,9 @@ public:
   float value;
   std::string exportedData;
   std::string exportedMessage;
+  std::string statistics;
 
-  ResultEntry(int id, std::string modelName, std::string subjectName, std::string notes, std::string date, float value, std::string exportedData, std::string exportedMessage) : id(id), modelName(modelName), subjectName(subjectName), notes(notes), date(date), value(value), exportedData(exportedData), exportedMessage(exportedMessage) { }
+  ResultEntry(int id, std::string modelName, std::string subjectName, std::string notes, std::string date, float value, std::string exportedData, std::string exportedMessage, std::string statistics) : id(id), modelName(modelName), subjectName(subjectName), notes(notes), date(date), value(value), exportedData(exportedData), exportedMessage(exportedMessage), statistics(statistics) { }
 };
 
 #endif
